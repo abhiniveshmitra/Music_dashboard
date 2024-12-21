0ed85043e5502a19c061574dc6842872c788fd9e
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2024-12-21 21:10:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ filtered_data = analyze_sentiment(filtered_data)
 
 # Artist Sentiment Search
 search_sentiment_analysis(filtered_data)
+
 # Expanded Artist Comparison
 if st.button("Compare Artists"):
     compare_artists(filtered_data)
