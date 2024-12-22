8b716dfc16185fde3dfdd766460103706b71cde0
abhiniveshmitra <133777817+abhiniveshmitra@users.noreply.github.com>
2024-12-22 13:36:08 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -6,11 +6,15 @@ import os
 
 @st.cache_data
 def load_data():
+    """
+    Downloads and loads the CSV (if not already present).
+    Filters only English lyrics.
+    """
     file_id = "1bw3EvezRiUj9sV3vTT6OtY840pxcPpW1"
     zip_output = 'ezyzip.zip'
     csv_output = 'filtered_rock_1950_2000_cleaned.csv'
     
-    # Download ZIP from Google Drive
+    # Download ZIP from Google Drive (if needed)
     if not os.path.exists(csv_output):
         gdown.download(f'https://drive.google.com/uc?id={file_id}&confirm=t', zip_output, quiet=False)
         with ZipFile(zip_output, 'r') as zip_ref:
